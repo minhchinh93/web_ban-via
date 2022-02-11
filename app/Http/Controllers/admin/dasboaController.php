177748239ef5481d4e48e0bfd2469b332c239f89
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2022-02-11 10:40:14 +0700

    okok

--- app/Http/Controllers/admin/dasboaController.php
+++ app/Http/Controllers/admin/dasboaController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class dasboaController extends Controller
 {
@@ -14,30 +15,57 @@ class dasboaController extends Controller
         // $totalbill = bill::sum('total');
         // $totalbill_detaill = bill_detaill::sum('quantity');
         // $totalcustomer = customer::all()->count();
-        $totalidea = Product::where('description', '<>', null)->count();
-        $totalDesign = Product::where('description', '<>', null)->count();
+        // $totalidea = Product::where('description', '<>', null)->count();
+        // $totalDesign = Product::where('description', '<>', null)->count();
         // $totalATM = bill::where('payment', 'ATM')->count();
 
-        // $tables = bill::join('bill_detaills', 'bill_detaills.id_bill', '=', 'bills.id')
-        //     ->join('customers', 'bills.id_User', '=', 'users.id')
-        //     ->select(DB::raw('SUM(bill_detaills.quantity) as "so_luong",
-        //         users.name as "khach_hang",
-        //         bills.total,bills.date_order,
-        //         bills.payment,
-        //         users.id,
-        //         users.address,
-        //         users.phone,
-        //         users.note'
-        //     ))
-        //     ->groupBy('bills.id')
-        //     ->get();
+        $Idea = User::join('products', 'products.id_idea', '=', 'users.id')
+            ->select(DB::raw('COUNT(products.id_idea) as "sum",
+            users.name as "name",
+            users.email as "email",
+            users.role as "role",
+            users.deleted_at as "deleted_at",
+            products.id_idea as "id"
+            '
+            ))
+            ->groupBy('products.id_idea')
+            ->withTrashed()
+            ->get();
+
+        // SELECT
+        // COUNT(product_png_details.id) as product_png_details,
+        // COUNT(mocup_products.id) as mocup_products, users.name, users.role
+        // FROM users
+        // INNER JOIN products ON products.User_id = users.id
+        // INNER JOIN product_png_details ON products.id = product_png_details.product_id
+        // INNER JOIN mocup_products ON products.id = mocup_products.product_id
+        // GROUP BY products.User_id;
+
+        $designer = User::join('products', 'products.User_id', '=', 'users.id')
+            ->join('product_png_details', 'product_png_details.product_id', '=', 'products.id')
+            ->join('mocup_products', 'mocup_products.product_id', '=', 'products.id')
+            ->select(DB::raw('COUNT(product_png_details.id) as "product_png_details",
+            COUNT(mocup_products.id) as "mocup_products",
+            users.name as "name",
+            users.email as "email",
+            users.role as "role",
+            users.deleted_at as "deleted_at",
+            products.User_id as "id"
+            '
+            ))
+            ->groupBy('products.User_id')
+            ->withTrashed()
+            ->get();
+        // dd($designer);
         // $shows = User::where('name', 'like', "%{$keyword}%")->withTrashed()->paginate(10);
-        $shows = Product::withTrashed()->paginate(100);
+
+        // $shows = User::withTrashed()->paginate(100);
         // dd($shows[4]->user->role);
 
         return view('admin/dasboa/index'
             , [
-                'shows' => $shows,
+                'shows' => $Idea,
+                'designer' => $designer,
                 //     'totalbill_detaill' => $totalbill_detaill,
                 //     'totalcustomer' => $totalcustomer,
                 //     'totalCOD' => $totalCOD,
--- resources/views/admin/dasboa/index.blade.php
+++ resources/views/admin/dasboa/index.blade.php
@@ -21,14 +21,6 @@
             </div>
             <div class="col-md-2 col-sm-2 box0">
                 <div class="box1">
-                    <span class="li_stack"></span>
-                    <h5>MEMBER IDEA: 10 </h5>
-                    <h4>MEMBER DESIGNER: 20</h4>
-                </div>
-                <a style="color:red" href="#"><p>see more !</p></a>
-            </div>
-            <div class="col-md-2 col-sm-2 box0">
-                <div class="box1">
                     <span class="li_news"></span>
                     <h5>TOTAL IDEA:</h5>
                     <h4><b>{{ null }} 1000</b></h4>
@@ -43,19 +35,28 @@
                 </div>
                 <a style="color:red" href="#"><p>see more !</p></a>
             </div>
+            <div class="col-md-2 col-sm-2 box0">
+                <div class="box1">
+                    <span class="li_stack"></span>
+                    <h5>MEMBER IDEA: 10 </h5>
+                    <h4>MEMBER DESIGNER: 20</h4>
+                </div>
+                <a style="color:red" href="#"><p>see more !</p></a>
+            </div>
         </div>
+
         <div class="row mt">
             <div class="col-md-12">
                 <div class="content-panel">
-                    <h4><i class="fa fa-angle-right"></i> MEMBER MANAGER </h4><hr><table class="table table-striped table-advance table-hover">
+                    <h4><i class="fa fa-angle-right"></i> IDEA MANAGER  </h4><hr><table class="table table-striped table-advance table-hover">
 
 
                         <thead>
                         <tr>
                             <th><i class="fa fa-bullhorn"></i> NAME</th>
+                            <th><i class="fa fa-bookmark"></i> EMAIL</th>
                             <th class="hidden-phone"><i class="fa fa-question-circle"></i> ROLE</th>
-                            <th><i class="fa fa-bookmark"></i> PRODUCT/DAY</th>
-                            <th><i class="fa fa-bookmark"></i> TOTAL PRODUCT</th>
+                            <th><i class="fa fa-bookmark"></i> TOTAL IDEA</th>
                             <th><i class=" fa fa-edit"></i> Status</th>
                             <th></th>
                         </tr>
@@ -63,16 +64,69 @@
                         <tbody>
                             @foreach ($shows as $show)
                         <tr>
-                            <td><a href="basic_table.html#">{{ $show->user->name?? null }}</a></td>
-                            @if($show->user->role ==1)
+                            <td><a href="basic_table.html#">{{ $show->name?? null }}</a></td>
+                            <td>{{$show->email}} </td>
+                            @if($show->role ==1)
                             <td><span class="label label-info label-mini">DESIGNER</span></td>
-                            @elseif ($show->user->role ==2)
+                            @elseif ($show->role ==2)
                             <td><span class="label label-warning label-mini">IDEA</span></td>
                             @else
                             <td><span class="label label-success label-mini">ADMIN</span></td>
                             @endif
-                             <td>{{$show->id_idea}} </td>
-                            <td>12000.00$ </td>
+                            <td><h4>{{$show->sum}}</h4></td>
+                            <td>
+                                @if (  $show->deleted_at ==  null)
+                                <span class="label label-info label-mini">active</span></td>
+                                @else
+                                <span class="label label-danger">disabled</span>
+                                @endif
+                            </td>
+                            <td>
+                                <span class="btn btn-success btn-xs">
+                                    <a style="color:white" href="{{ route('DetailMember',[$show->id]) }}">
+                                     Detail
+                                  </a>
+                                 </span>
+                                {{-- <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button> --}}
+                                {{-- <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button> --}}
+                            </td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div><!-- /content-panel -->
+            </div><!-- /col-md-12 -->
+        </div>
+        <div class="row mt">
+            <div class="col-md-12">
+                <div class="content-panel">
+                    <h4><i class="fa fa-angle-right"></i> DESIGNER MANAGER  </h4><hr><table class="table table-striped table-advance table-hover">
+
+
+                        <thead>
+                        <tr>
+                            <th><i class="fa fa-bullhorn"></i> NAME</th>
+                            <th class="hidden-phone"><i class="fa fa-question-circle"></i> ROLE</th>
+                            <th><i class="fa fa-bookmark"></i> TOTAL MOCKUP</th>
+                            <th><i class="fa fa-bookmark"></i> TOTAL PNG</th>
+                            <th><i class=" fa fa-edit"></i> Status</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($designer as $show)
+                        <tr>
+                            <td><a href="basic_table.html#">{{ $show->name?? null }}</a></td>
+                            @if($show->role ==1)
+                            <td><span class="label label-info label-mini">DESIGNER</span></td>
+                            @elseif ($show->role ==2)
+                            <td><span class="label label-warning label-mini">IDEA</span></td>
+                            @else
+                            <td><span class="label label-success label-mini">ADMIN</span></td>
+                            @endif
+                             <td>{{$show->mocup_products}} </td>
+                             <td>{{$show->product_png_details}} </td>
                             <td>
                                 @if (  $show->deleted_at ==  null)
                                 <span class="label label-info label-mini">active</span></td>
