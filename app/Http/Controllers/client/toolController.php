dff9790236043c6324872b3f99db790ab773e73b
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2022-03-12 11:12:11 +0700

    add laij codde

--- app/Http/Controllers/client/toolController.php
+++ app/Http/Controllers/client/toolController.php
@@ -10,19 +10,114 @@ use App\Models\type_product;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class toolController extends Controller
 {
     //
-    public function showTool()
+    public function showTool(Request $request)
     {
         $show = cornerstone::all();
         $users = User::all();
+        Carbon::setLocale('vi');
+        $now = Carbon::now('Asia/Ho_Chi_Minh');
+        $show = cornerstone::all();
+        $times = $now->toDateString();
+        $designer = User::get()->where('role', 2);
+        $type_product = type_product::get();
+        $size = size::get();
+        $keyword = $request->keyword;
 
-        return view('client.dasboa.tool', [
-            'shows' => $show,
-            'users' => $users,
-        ]);
+        if ($request->cornerstone != null) {
+            $report = Product::join('product_cornerstone', 'product_cornerstone.id_product', '=', 'products.id')
+                ->select(DB::raw('
+        COUNT(product_cornerstone.cornerstones_id),
+        products.id as "id",
+        products.id_type as "id_type",
+        products.User_id as "User_id",
+        products.id_idea as "id_idea",
+        products.size_id as "size_id",
+        products.title as "title",
+        products.description as "description",
+        products.ImagePNG as "ImagePNG",
+        products.status as "status",
+        products.action as "action",
+        products.created_at as "created_at",
+        products.updated_at as "updated_at"
+        '
+                ))
+                ->groupBy('products.id')
+                ->Where('status', "5")
+                ->Where('product_cornerstone.cornerstones_id', $request->cornerstone)
+            // ->Where('title', 'like', "%{$keyword}%")
+                ->paginate(1000);
+
+        } else {
+            $report = Product::orderBy('updated_at', 'desc')
+                ->Where('status', "5")
+                ->Where('title', 'like', "%{$keyword}%")
+                ->paginate(10);
+        }
+
+        // foreach ($report as $rep) {
+        //     foreach ($rep->cornerstones as $corner) {
+        //         $name[] = $corner->name;
+        //     }
+        // }
+
+        {
+            if ($report->total() > 0) {
+                foreach ($report as $rep) {
+                    $userIdeas[] = User::where('id', $rep->id_idea)->get();
+                }
+                foreach ($userIdeas as $userIdea) {
+                    $name[] = $userIdea;
+                }
+
+            } else {
+                $name = "";
+            }
+        }
+
+        if ($report->total() != 0) {
+            foreach ($report as $billdd) {
+                $dt[] = Carbon::create($billdd->created_at);
+            }
+
+            foreach ($dt as $key) {
+                $now = Carbon::now('Asia/Ho_Chi_Minh');
+                $time[] = $key->diffForHumans($now);
+            }
+        } else {
+            $time = '';
+
+        }
+        // dd($time[4]);
+        // dd(count($report[0]->mocups));
+
+        $totalDone = Product::orderBy('id', 'desc')->where('status', 5)->count();
+        $totalPending = Product::orderBy('id', 'desc')->where('status', 3)->count();
+        $totalNotReceived = Product::orderBy('id', 'desc')->where('status', 1)->count();
+        $totalallidea = Product::orderBy('id', 'desc')->count();
+        return view('client.dasboa.tool',
+            ['designers' => $designer,
+                'reports' => $report,
+                'totalDone' => $totalDone,
+                'totalPending' => $totalPending,
+                'totalNotReceived' => $totalNotReceived,
+                'type_products' => $type_product,
+                'totalallidea' => $totalallidea,
+                'times' => $time,
+                'sizes' => $size,
+                'name' => $name,
+                'shows' => $show,
+                'users' => $users,
+
+            ]);
+        // return view('client.dasboa.tool', [
+        //     'shows' => $show,
+        //     'users' => $users,
+        // ]);
     }
     public function postTypeProduct(Request $request)
     {
--- resources/views/client/dasboa/tool.blade.php
+++ resources/views/client/dasboa/tool.blade.php
@@ -6,9 +6,9 @@
 <section id="main-content">
     <section class="wrapper">
         <div class="row mt">
-            <div class="col-lg-12">
+            <div class="col-lg-3">
                 <div class="form-panel">
-                  <h4 class="mb"><i class="fa fa-angle-right"></i> tao loai sp</h4>
+                  <h4 class="mb"><i class="fa fa-angle-right"></i> Category</h4>
                     <form class="form-inline"action="{{ route('postTypeProduct') }}" method="post" >
                         @csrf
                     <div class="form-group">
@@ -21,11 +21,10 @@
                 </form>
                 </div><!-- /form-panel -->
             </div><!-- /col-lg-12 -->
-        </div>
-        <div class="row mt">
-            <div class="col-lg-12">
+
+            <div class="col-lg-3">
                 <div class="form-panel">
-                  <h4 class="mb"><i class="fa fa-angle-right"></i> nen tang</h4>
+                  <h4 class="mb"><i class="fa fa-angle-right"></i> cornerstone</h4>
                     <form class="form-inline"action="{{ route('cornerstone') }}" method="post" >
                         @csrf
                     <div class="form-group">
@@ -38,9 +37,8 @@
                 </form>
                 </div><!-- /form-panel -->
             </div><!-- /col-lg-12 -->
-        </div>
-        <div class="row mt">
-            <div class="col-lg-6">
+
+            <div class="col-lg-3">
                 <div class="form-panel">
                   <h4 class="mb"><i class="fa fa-angle-right"></i> add cornerstone</h4>
                   <form class="form-inline" action="{{ route('cornerstoneadd') }}" method="post">
@@ -59,7 +57,7 @@
             </form>
                 </div><!-- /form-panel -->
             </div><!-- /col-lg-12 -->
-            <div class="col-lg-6">
+            <div class="col-lg-3">
                 <div class="form-panel">
                   <h4 class="mb"><i class="fa fa-angle-right"></i> xoa cornerstone</h4>
                   <form class="form-inline" action="{{ route('cornerstonedele') }}" method="post">
@@ -78,6 +76,239 @@
             </form>
                 </div><!-- /form-panel -->
             </div><!-- /col-lg-12 -->
+        </div>
+
+
+
+        <div class="row mt" >
+            <div class="col-md-12" >
+                <div class="content-panel">
+                    <h4><i class="fa fa-angle-right"></i>  Bảng Báo Cáo</h4>
+                    <div class="col-lg-4">
+                        <h4 style="margin-left: 2%;" class="category"><a style="color: gray" href="#"> Hoàn thành ({{ $totalDone ?? null}}) </a>
+                             | <a  style="color: rgb(13, 182, 36)" href="#">chờ duyệt ({{ $totalPending ?? null}})</a>
+                             | <a style="color:red" href="#">chưa nhận ({{ $totalNotReceived ?? null}})</a>
+                             | <a style="color:red" href="#"> tất cả ({{ $totalallidea ?? null}})</a>
+
+                            </h4>
+                    </div><!-- /col-lg-12 -->
+                    <div class="col-lg-4">
+                        <form class="form-inline" >
+                            <select name="cornerstone" id="cars" style="border-radius: 15px;" class="form-control">
+                            @foreach ($shows as $show)
+                                <option value="{{ $show->id }}">{{  $show->name }}</option>
+                            @endforeach
+                        </select>
+                        <button type="submit" class="btn btn-theme"><i class="fa-solid fa-paper-plane"></i></button>
+                    </form>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <form class="form-inline" role="form">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="exampleInputEmail2">tim kiem</label>
+                                        <input type="text" class="form-control" name="keyword" aria-label=" Search" id="exampleInputEmail2" value="{{ request()->keyword }}" placeholder="tim kiem">
+                                    </div>
+                                    <button type="submit" class="btn btn-theme">tim kiem</button>
+                                </form>
+                            </div>
+                        </div>
+                   </div><!-- /col-lg-12 -->
+
+
+                    <table class="table table-striped table-advance table-hover">
+                        <thead>
+                        <tr>
+                            <th><i class="fa fa-bullhorn"></i> Designer</th>
+                            <th><i class="fa fa-bullhorn"></i> Category(size)</th>
+                            <th><i class="fa fa-bullhorn"></i> Title</th>
+                            <th><i class="fa fa-bullhorn"></i> cornerstones</th>
+                            <th class="hidden-phone"><i class="fa fa-question-circle"></i> Description </th>
+                            <th class="hidden-phone"><i class="fa fa-question-circle"></i>Time</th>
+                            <th><i class="fa fa-bookmark"></i>Idea </th>
+                            <th><i class=" fa fa-edit"></i> Mockup </th>
+                            <th><i class=" fa fa-edit"></i> PNG </th>
+                            <th><i class=" fa fa-edit"></i>  Status </th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        @php
+                          $i=0
+                        @endphp
+                        <tbody>
+                            @foreach ($reports as  $report)
+                            <tr>
+                                <td><a href="#">{{$report->user->name ?? null }}</a></td>
+                                <td>{{ $report->type_product->name ?? null }}<b>({{ $report->size->name ?? null  }})</b></td>
+                                <td  style=" max-width: 150px;"><b>{{ $report->title ?? null }}</b></td>
+                                <td  style=" max-width: 150px;"><br>@foreach ($report->cornerstones as $cornerstone)
+                                    <span class="label label-info label-mini">{{ $cornerstone->name}}</span><br/><br/>
+                                    @endforeach
+                                    <form class="form-inline" action="{{ route('cornerstoneProduct',[$report->id]) }}" method="post">
+                                        @csrf
+                                        <select name="cornerstone" id="cars" style="border-radius: 15px;" class="form-control">
+                                        @foreach ($shows as $show)
+                                            <option value="{{ $show->id }}">{{  $show->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <button type="submit" style="border-radius: 10px;" class="btn btn-theme"><i class="fa-solid fa-paper-plane"></i></button>
+                                </form>
+                                    </b></td>
+                                <td class="hidden-phone"
+                                style=" max-width: 300px;
+                                color:black;
+                                overflow: hidden;
+                                text-overflow: ellipsis;
+                               word-wrap: break-word;">{!!  $report->description ?? null !!}
+                                    <form class="form-inline" action="{{ route('comment',[$report->id]) }}" method="post">
+                                  @csrf
+                                      <div class="form-group">
+                                            <input type="text" class="form-control" id="exampleInputEmail2" name="comment" placeholder="comment">
+                                        </div>
+                                        <button type="submit" class="btn btn-theme">gửi</button>
+                                    </form>
+                                </td>
+                                <td><a href="#">{{ $report->created_at ?? null }}</a></td>
+                                @if(count($report->product_details)!=0)
+                                <td data-toggle="modal" data-target="#a{{$report->id}}"><img src="{{asset('/storage/'.$report->product_details[0]->ImageDetail)}}" style="width: 150px; height :150px;  border-radius: 5%;" >
+                                  @else
+                                <td data-toggle="modal" data-target="#a{{$report->id}}"></td>
+                                @endif
+                                </td>
+                                <div class="modal fade" id="a{{$report->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                    <section id="main-content">
+                                        <section class="wrapper">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                      <div class="modal-content">
+                                        <div class="modal-header">
+                                          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                          </button>
+                                        </div>
+                                          @foreach ($report->product_details as $rep)
+                                          <div class="project-wrapper">
+                                            <div class="project">
+                                                <a href="{{ route('deleteImage',[$rep->id]) }}"><span class="label label-info label-mini">xoa</span></a>
+                                                <div class="photo-wrapper">
+                                                    <div class="photo">
+                                                        <a class="fancybox" target="_blank" href="{{asset('/storage/'.$rep->ImageDetail)}}" alt="" ><img src="{{asset('/storage/'.$rep->ImageDetail)}}"  width="100%"></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                          @endforeach
+                                        <div class="modal-footer">
+                                          <form class="form-inline" action="{{ route('addImage',[$report->id]) }}" method="post" enctype="multipart/form-data">
+                                            @csrf
+                                            <input name="image[]"  type="file" multiple required>
+                                          <button type="submit" class="btn btn-primary" >Add Image Idea</button>
+                                        </form>
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                </section>
+                                </section>
+                                  </div>
+                                  @if (count($report->mocups)!=0)
+                                  <td data-toggle="modal" data-target="#c{{$report->id}}"><img src="{{asset('/storage/'.$report->mocups[0]->mocup)}}" style="width: 150px; height :150px;  border-radius: 5%;" ></td>
+                                    @else
+                                    <td data-toggle="modal" data-target="#c{{$report->id}}"></td>
+                                   @endif
+                                <div class="modal fade" id="c{{$report->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                    <section id="main-content">
+                                        <section class="wrapper">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                      <div class="modal-content">
+                                        <div class="modal-header">
+                                          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                          </button>
+                                        </div>
+
+                                          @foreach ($report->mocups as $rep)
+                                          <div class="project-wrapper">
+                                            <div class="project">
+                                                <div class="photo-wrapper">
+                                                    <div class="photo">
+                                                        <a class="fancybox" target="_blank" href="{{asset('/storage/'.$rep->mocup)}}" alt="" ><img src="{{asset('/storage/'.$rep->mocup)}}"  width="100%"></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                          @endforeach
+                                        <div class="modal-footer">
+                                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                </section>
+                                </section>
+                                  </div>
+                                <td data-toggle="modal" data-target="#b{{$report->id}}">
+                                    @if (count($report->ProductPngDetails)!=0)
+                                    <img src="{{asset('/storage/'.$report->ProductPngDetails[0]->ImagePngDetail ) ?? null }}" style="border-radius: 5%;width: 150px; height :150px"  >
+                                    @endif
+                                    {{-- <span type="button" class="label label-success" data-toggle="modal" data-target="#b{{$report->id}}">
+                                        xem anh designer
+                                      </span> --}}
+                                </td>
+                                <div class="modal fade" id="b{{$report->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                    <section id="main-content">
+                                        <section class="wrapper">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                      <div class="modal-content">
+                                        <div class="modal-header">
+                                          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                          </button>
+                                        </div>
+
+                                          @foreach ($report->ProductPngDetails as $rep)
+                                          <div class="project-wrapper">
+                                            <div class="project">
+                                                <div class="photo-wrapper">
+                                                    <div class="photo">
+                                                        <a class="fancybox" target="_blank" href="{{asset('/storage/'.$rep->ImagePngDetail)}}" alt="" ><img src="{{asset('/storage/'.$rep->ImagePngDetail)}}"  width="100%"></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                          @endforeach
+                                        <div class="modal-footer">
+                                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                                        </div>
+                                      </div>
+                                    </div>
+                                </section>
+                                </section>
+                                  </div>
+
+
+                                @if ($report->status == 1)
+                                <td><span class="label label-warning label-mini">chưa nhận</span></td>
+                                @elseif ( $report->status == 2)
+                                <td><span class="label label-info label-mini">đã nhận</span></td>
+                                @elseif ( $report->status == 3)
+                                <td><span class="label label-info label-mini">chờ duyệt</span></td>
+                                @elseif ( $report->status == 4)
+                                <td><span class="label label-warning label-mini">làm lại</span></td>
+                                @else
+                                <td><span class="label label-success label-mini">hoàn thành</span></td>
+                                @endif
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div><!-- /content-panel -->
+            </div><!-- /col-md-12 -->
+            {{ $reports->links() }}
         </div>
     </section>
 </section>
