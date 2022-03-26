bd04e778cfddfae99f192fb48dc979e313fbb922
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2022-03-26 12:07:18 +0700

    add laij codde

---
+++ app/Http/Controllers/client/finePngController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\client;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductPngDetails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class finePngController extends Controller
+{
+    //SELECT
+    // users.name as "name",
+    // products.title as "title",
+    // product_png_details.ImagePngDetail as "ImagePngDetail"
+    // FROM product_png_details
+    // INNER JOIN products ON product_png_details.product_id = products.id
+    // INNER JOIN users ON products.id_idea = users.id
+    public function findPNG(Request $request)
+    {
+        $keyword = $request->keyword;
+        $show = ProductPngDetails::join('products', 'product_png_details.product_id', '=', 'products.id')
+        // INNER JOIN users ON products.id_idea = users.id
+            ->join('users', 'products.id_idea', '=', 'users.id')
+            ->select(DB::raw('
+            users.name as "name",
+            products.title as "title",
+            product_png_details.ImagePngDetail as "ImagePngDetail"
+            '
+            ))
+            ->Where('product_png_details.Sku', 'like', "%{$keyword}%")
+            ->paginate(10);
+
+
+        return view('client.findPNG.indexPNG', ['shows' => $show,
+        ]);
+    }
+}
---
+++ resources/views/client/findPNG/indexPNG.blade.php
@@ -0,0 +1,51 @@
+@extends('client.app')
+
+
+@section ('content')
+<section id="main-content">
+    <section class="wrapper" style="color:black; font-family:Roboto,sans-serif; background-image: url('https://allimages.sgp1.digitaloceanspaces.com/wikilaptopcom/2021/01/Background-tim-cuc-dep.png');background-size: cover;" >
+
+    <div class="row mt">
+        <div class="col-lg-6">
+            {{-- <form action="{{ route('find') }}" class="form-inline"role="form" method="GET"> --}}
+            <form  class="form-inline"role="form" >
+              @csrf
+                <select class="form-control "  id="cars" name="type" >
+                    <option value=" ">Tìm Kiếm loại</option>
+                </select>
+                  <button type="submit" class="btn btn-theme "><i class="fa-solid fa-magnifying-glass"></i> </button>
+            </form>
+        </div>
+        <div class="col-lg-6">
+            <form class="form-inline" role="form">
+                <div class="form-group">
+                    <label class="sr-only" for="exampleInputEmail2">tim kiem</label>
+                    <input type="text" class="form-control" name="keyword" aria-label=" Search" id="exampleInputEmail2" value="{{ request()->keyword }}" placeholder="tim kiem">
+                </div>
+                <button type="submit" class="btn btn-theme"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+    </div>
+    <div class="row mt">
+        @foreach($shows as $show)
+        <div class="col-lg-4 col-md-4 col-sm-4 mb">
+            <div class="content-panel pn "  style="display: flex;flex-direction: space-between;">
+                <div class="media"  style="margin-right:20px">
+                    <img src="{{asset('/storage/'.$show->ImagePngDetail)}}" alt="..." width="200px" class="img-thumbnail">
+                </div>
+                <div class="media-body">
+                      <h3 class="mt-0">Idea: {{ $show->name}}</h3><hr>
+                      <h4>Title: {{ $show->title}}</h4><hr>
+
+                </div>
+                </div>
+            </div><!-- --/panel ---->
+        </div>
+        @endforeach
+    </div>
+</section>
+</section>
+
+@endsection
+
+
