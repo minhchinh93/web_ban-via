1bb5c5749547d5aeb9c365622d3c6ea776e8d4ea
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2022-03-31 12:13:24 +0700

    add laij codde

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -42,157 +42,19 @@
       <!--header start-->
       <header class="header black-bg" style="background-color: #ff865c;">
               <div class="sidebar-toggle-box">
-                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+                <div data-placement="right" data-original-title="Toggle Navigation" ><img  src="http://hblmedia.online/admin/img/Logo.png" width="55"style="margin-top:-15px;border-radius:30%" class=" fa fa-bars tooltips"></div>
               </div>
             <!--logo start-->
             <a href="index.html" class="logo"><b>MEDIA - System Mr.Hải</b></a>
             <!--logo end-->
             <div class="nav notify-row" id="top_menu">
-                <!--  notification start -->
-                <ul class="nav top-menu">
-                    <!-- settings start -->
-                    <li class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="index.html#">
-                            <i class="fa fa-tasks"></i>
-                            <span class="badge bg-theme">4</span>
-                        </a>
-                        <ul class="dropdown-menu extended tasks-bar">
-                            <div class="notify-arrow notify-arrow-green"></div>
-                            <li>
-                                <p class="green">You have 4 pending tasks</p>
-                            </li>
-                            <li>
-                                <a href="index.html#">
-                                    <div class="task-info">
-                                        <div class="desc">DashGum Admin Panel</div>
-                                        <div class="percent">40%</div>
-                                    </div>
-                                    <div class="progress progress-striped">
-                                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                            <span class="sr-only">40% Complete (success)</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="index.html#">
-                                    <div class="task-info">
-                                        <div class="desc">Database Update</div>
-                                        <div class="percent">60%</div>
-                                    </div>
-                                    <div class="progress progress-striped">
-                                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-                                            <span class="sr-only">60% Complete (warning)</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="index.html#">
-                                    <div class="task-info">
-                                        <div class="desc">Product Development</div>
-                                        <div class="percent">80%</div>
-                                    </div>
-                                    <div class="progress progress-striped">
-                                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                                            <span class="sr-only">80% Complete</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="index.html#">
-                                    <div class="task-info">
-                                        <div class="desc">Payments Sent</div>
-                                        <div class="percent">70%</div>
-                                    </div>
-                                    <div class="progress progress-striped">
-                                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
-                                            <span class="sr-only">70% Complete (Important)</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="external">
-                                <a href="#">See All Tasks</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <!-- settings end -->
-                    <!-- inbox dropdown start-->
-                    <li id="header_inbox_bar" class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="index.html#">
-                            <i class="fa fa-envelope-o"></i>
-                            <span class="badge bg-theme">5</span>
-                        </a>
-                        <ul class="dropdown-menu extended inbox">
-                            <div class="notify-arrow notify-arrow-green"></div>
-                            <li>
-                                <p class="green">You have 5 new messages</p>
-                            </li>
-                            <li>
-                                <a href="index.html#">
-                                    <span class="photo"><img alt="avatar" src="{{ asset('admin/img/ui-zac.jpg"') }}></span>
-                                    <span class="subject">
-                                    <span class="from">Zac Snider</span>
-                                    <span class="time">Just now</span>
-                                    </span>
-                                    <span class="message">
-                                        Hi mate, how is everything?
-                                    </span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="index.html#">
-                                    <span class="photo"><img alt="avatar" src="{{ asset('admin/img/ui-divya.jpg') }}"></span>
-                                    <span class="subject">
-                                    <span class="from">Divya Manian</span>
-                                    <span class="time">40 mins.</span>
-                                    </span>
-                                    <span class="message">
-                                     Hi, I need your help with this.
-                                    </span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="index.html#">
-                                    <span class="photo"><img alt="avatar" src="{{ asset('admin/img/ui-danro.jpg') }}"></span>
-                                    <span class="subject">
-                                    <span class="from">Dan Rogers</span>
-                                    <span class="time">2 hrs.</span>
-                                    </span>
-                                    <span class="message">
-                                        Love your new Dashboard.
-                                    </span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="index.html#">
-                                    <span class="photo"><img alt="avatar" src="{{ asset('admin/img/ui-sherman.jpg') }}"></span>
-                                    <span class="subject">
-                                    <span class="from">Dj Sherman</span>
-                                    <span class="time">4 hrs.</span>
-                                    </span>
-                                    <span class="message">
-                                        Please, answer asap.
-                                    </span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="index.html#">See all messages</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <!-- inbox dropdown end -->
-                </ul>
-                <!--  notification end -->
             </div>
             <div class="top-menu">
             	<ul class="nav pull-right top-menu">
                     @if(Auth::check())
-                    <li><a class="logout" href="{{ route('logout') }}">Logout</a></li>
+                    <li><a class="logout" href="{{ route('logout') }}">LOGOUT</a></li>
                     @else
-                    <li><a class="logout" href="{{ route('login') }}">Đăng Nhập</a></li>
+                    <li><a class="logout" href="{{ route('login') }}">LOGIN</a></li>
                     @endif
             	</ul>
             </div>
@@ -204,23 +66,22 @@
       *********************************************************************************************************************************************************** -->
       <!--sidebar start-->
       <aside>
-          <div id="sidebar"  class="nav-collapse " style="background-color:#24292f">
+          <div id="sidebar"  class="nav-collapse " style="background-color:#24292f;max-width:70px">
               <!-- sidebar menu start-->
               <ul class="sidebar-menu" id="nav-accordion">
 
-              	  <p class="centered"><a href="profile.html"><img src="http://hblmedia.online/admin/img/Logo.png" class="img-circle" width="60"></a></p>
-              	  <h5 class="centered">Chào Mừng Admin !</h5>
+              	  {{-- <p class="centered"><a href="profile.html"><img src="http://hblmedia.online/admin/img/Logo.png" class="img-circle" width="60"></a></p>
+              	  <h5 class="centered">Chào Mừng Admin !</h5>--}}
 
-                  <li class="mt">
+                 <li class="mt">
                       <a class="{{ (request()->is('admin/AadminHome')) ? 'active' : 'sub-menu' }}" href="{{ route('AadminHome') }}">
                           <i class="fa fa-dashboard"></i>
-                          <span>Dashboard</span>
+     
                       </a>
                   </li>
                   <li class="sub-menu">
                       <a class="{{ (request()->is('admin/showList/dasboa')) ? 'active' : 'sub-menu' }}" href="{{ route('showdasboa') }}">
                           <i class="fa fa-dashboard"></i>
-                          <span>statistics</span>
                       </a>
                   </li>
 
@@ -228,13 +89,11 @@
                       <a 	class="{{ (request()->is('admin/showList/User')) ? 'active' : 'sub-menu' }}"
                         href="{{ route('showUser') }}" >
                           <i class="fa fa-desktop"></i>
-                          <span>USER</span>
                       </a>
                   </li>
                   <li class="sub-menu">
                     <a class="{{ (request()->is('/showDoc')) ? 'active' : 'sub-menu' }}" href="{{ route('showDoc') }}" >
                         <i class="fa fa-th"></i>
-                        <span>Document </span>
                     </a>
                 </li>
               </ul>
