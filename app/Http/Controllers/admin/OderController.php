48ffd41325b13bf1fe8ecd9635f54fb0b99c5e9b
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2022-04-02 12:00:47 +0700

    add laij codde

--- app/Http/Controllers/admin/OderController.php
+++ app/Http/Controllers/admin/OderController.php
@@ -33,10 +33,52 @@ class OderController extends Controller
             ->distinct()
             ->groupBy("users.name")
             ->get();
+        //total number
+        $totalItemEsty = Oder_detail::all()->sum('Number_Items');
+        $totalPriceEsty = Oder_detail::all()->sum('Order_Total');
+        $totalItemEBay = oder_Ebay::all()->sum('Number_Items');
+        $totalPriceEBay = oder_Ebay::all()->sum('Order_Total');
+        // dd($totalItemEBay);
+        //chart
+        $totalsicharEsty = Oder_detail::select(DB::raw('
 
+        sum(oder_details.order_Total) as "order_Total"
+        '
+        ))
+            ->groupBy('Sale_Date')
+            ->paginate(30);
+        // $totalItemEsty = Oder_detail::all()->groupBy('Sale_Date');
+        foreach ($totalsicharEsty as $totalsichar) {
+            $sicharEsty[] = $totalsichar->order_Total;
+        }
+
+        $StrsicharEsty = implode(",", $sicharEsty);
+        $totalsicharEbay = oder_Ebay::select(DB::raw('
+
+        sum(oder__ebays.order_Total) as "order_Total"
+        '
+        ))
+            ->groupBy('Sale_Date')
+            ->paginate(30);
+        foreach ($totalsicharEbay as $totalsichar) {
+            $sicharEbay[] = $totalsichar->order_Total;
+        }
+        // $totolEsty = Oder_detail::paginate(30)->sum('Order_Total');
+        $totolEsty = array_sum($sicharEsty);
+        $totolEbay = array_sum($sicharEbay);
+
+        $StrsicharEbay = implode(",", $sicharEbay);
         return view('admin.Oder.IndexOderadmin', [
             'estys' => $esty,
             'ebays' => $ebay,
+            'totalItemEsty' => $totalItemEsty,
+            'totalPriceEsty' => $totalPriceEsty,
+            'totalItemEBay' => $totalItemEBay,
+            'totalPriceEBay' => $totalPriceEBay,
+            'StrsicharEsty' => $StrsicharEsty,
+            'StrsicharEbay' => $StrsicharEbay,
+            'totolEsty' => $totolEsty,
+            'totolEbay' => $totolEbay,
         ]);
 
     }
