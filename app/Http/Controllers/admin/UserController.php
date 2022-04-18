1b7a1a28467e4bb2d4e9f0ea8e541f681616ad53
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2022-04-18 10:33:03 +0700

    sua code

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -204,7 +204,7 @@ class UserController extends Controller
     }
     public function detailUserPending($id)
     {
-        dd($id);
+
         $report = Product::orderBy('updated_at', 'desc')->where('User_id', $id)->where('status', 4)->paginate(10);
         $totalPending = Product::orderBy('updated_at', 'desc')->where('User_id', $id)->where('status', 4)->count();
         $totalDone = Product::orderBy('updated_at', 'desc')->where('User_id', $id)->where('status', 5)->count();
