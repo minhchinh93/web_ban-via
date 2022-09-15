76d640616c0cf790df745ec29cb500f89d3d94fd
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2022-09-15 12:27:11 +0700

    sellewit

--- app/Helpers/SellerWix.php
+++ app/Helpers/SellerWix.php
@@ -251,4 +251,45 @@ class SellerWix
         echo $response;
 
     }
+    public function gettracking($id)
+    {
+        $curl = curl_init();
+        ini_set('max_execution_time', 300); //300 seconds = 5 minutes
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://www.ups.com/track/api/Track/GetStatus?loc=en_US',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{"Locale":"en_US","TrackingNumber":["' . $id . '"],"Requester":"st/trackdetails","returnToValue":""}',
+            CURLOPT_HTTPHEADER => array(
+                'authority: www.ups.com',
+                'accept: application/json, text/plain, */*',
+                'accept-language: en-US,en;q=0.9,vi;q=0.8',
+                'content-type: application/json',
+                'cookie: CONSENTMGR=consent:true%7Cts:1651131330431; bm_sz=1A4A1AB806814A158E081635DCFDB3DB~YAAQdetGaIHilwCDAQAANs7oPhEMVtme3jDdP/b71sb5Xw0lzvs+ioVUdJgLY+kWC4OGYUcTKnaHB65ctX1sUzAuyeYKvuUo80xvGqk8XkRhamhjhpEcI5gMMq/TKlZbBBAeOtOmYV4itNxDSA8PTIhIF8aUCKI7dv5qgTIuG0C35pIyIWcZN/iUfhK/kYfnvBpRCHUvD0R3DxXLUA8mTQQDjYa4bpQkmcB0XFaHfpuRIOgiwDfLKFHhOxOc/8pETNRkoFko/x68GI+q4Z/sBHRYJUOOhPy0PCr6+qLRRFI=~3682865~3617606; at_check=true; AMCVS_036784BD57A8BB277F000101%40AdobeOrg=1; AMCV_036784BD57A8BB277F000101%40AdobeOrg=-2121179033%7CMCIDTS%7C19251%7CMCMID%7C41490525932510522522216816084088740080%7CMCAAMLH-1663812588%7C3%7CMCAAMB-1663812588%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1663214988s%7CNONE%7CMCCIDH%7C1429808140%7CvVersion%7C5.3.0; _gcl_au=1.1.1896907940.1663207789; ak_bmsc=0DF48CD87E9540738060274174ED6B76~000000000000000000000000000000~YAAQdetGaIrilwCDAQAAKNPoPhG1CpqbrBW15SzIWLAwmupGEVNV53lECgTSKhRfPijQVqDirvCY6ceRFMHRjVC/U0OVhQogu32N3UJXcpzS8E9jdyMgqjfVh1jAy7x2D+7DsJrYq/8scLdYU11v9U3Le6uMXvO8y7EkViknZ6bmzaNFjCB9OMJ4uq30rYPqUHc9VzH8J7Sj8f/z2gONX6FCRf8v/gDLJ6taImZ44ulVzBX9fRLLooAtcpEOu5gDfRmwwujMZ0AHKFMHGUoXg+/zLIUo/H0VlrCdyINacAKCX4WllVC6FpZHvYkwNF8/JI7EikxuJT3F2lBCcGdHIrNdiGcPHiXwiuzSWGloJ830ysSYlwM0G54sX6S7V7aK2chZrEANqONXe12e4g0jMWIS3ANKJmr6qx3U1PB1eTciQHACy6ymwANHqZgCSRS4f1FOzBwvuq2jg3IHopExgMO1zeXvLJSAMDPf18Jk0MPBa1mXWs7KDjI=; s_cc=true; akacd_RWASP-default-phased-release=3840660590~rv=92~id=71f34ffb53624b3565f9444922854e54; _fbp=fb.1.1663207791443.1082097583; PIM-SESSION-ID=umDP8Q4f9TsUZR6c; aam_cms=segments%3D22945449%7C9625302; aam_uuid=41217697746060091122190056634914074932; mboxEdgeCluster=38; s_vnum=1664557200392%26vn%3D2; s_invisit=true; dayssincevisit_s=Less%20than%201%20day; st_cur_page=st_track; AKA_A2=A; alertsHide=true; X-CSRF-TOKEN=CfDJ8Jcj9GhlwkdBikuRYzfhrpIW8aDJS0oFLIzxs7nu0MzB8YEWoYpV_cwsNcLhzEqnElSdH514uBvfUGSRH6nkrHj4U6zRfuuS88CN3RuOlyAGtBdmj4ijSSsX-kKqQCfe1DAo9Y5q8tI5JMzipiRo8Uk; JSESSIONID=925CB6D4CBC23D324707B4D0FE8F60CB; _abck=742CC5F19E21F4A55594EB91A3B179C6~0~YAAQdetGaPvnmACDAQAAxXMuPwhnrZeGdTSEKrEJcWfWQuHdYtrAiNk6V1EfZbV2LREQqg5/3eRKEF5g4cfkYr7zAgBYXX5MIoM4A6cRGDN1cC44niqb/DHzMev5dcyuBYnG1lhq/jNe1hkIeXaTMI/Xmtz8+gDLnvG3omnWrgctW0UpUeHUzRCAP6hERQIhCGoMb/VT11aLI40ZH0Fz3YpeNOfame7CKMmLiL107i/etVSrj04+Fa5ASpXJB4iK7OS3BTGaeKYDUSdiBsp6Mlnc7G6iUaUI+fg0B42hl1h7Vsjtd+u6WCroiRyz+zpuVYGvEZ4dYbEb+uEtFi435kZvpUgl/5IB439G6AmvwbA6Uh1PKWfajWST937gUGqqItNlTM8D1zKMq8IrsTCF4uWq~-1~-1~-1; s_nr=1663212466130-Repeat; dayssincevisit=1663212466131; X-XSRF-TOKEN-ST=CfDJ8Jcj9GhlwkdBikuRYzfhrpLnLyLrXcKf4wpS54hFHt1dFKC-kwZMNbI8B1wxFM27SaJkTCscMA5RHdXIWXgoPPt052ZR3SRTkvUav2280jdwhGUicV-8ztCKtiJiZOe7HjcD6aHk_OtEc5Cvu6f1x4k; mbox=PC#0291a2b7236445348cd7444cf529fa6d.38_0#1726457271|session#c0bec165242b4d208b4aeab113a9e351#1663214331; utag_main=v_id:01806f18af75002231e952a32c6005074004706c00bd0$_sn:4$_se:31$_ss:0$_st:1663214270316$vapi_domain:ups.com$dc_visit:2$_prevpage:ups%3Aus%3Aen%3Atrack%3Bexp-1663215951827$_prevpageid:tracking%2FtrackWeb%2Ftra(3det).html%3Bexp-1663215951828$ses_id:1663210090526%3Bexp-session$_pn:16%3Bexp-session$fs_sample_user:undefined%3Bexp-session$dc_event:14%3Bexp-session$dc_region:ap-east-1%3Bexp-session; RT="z=1&dm=ups.com&si=67581ea4-0530-489b-9346-743c9fd446af&ss=l82ez1ok&sl=k&tt=tfu&bcn=%2F%2F684d0d4c.akstat.io%2F"; _abck=742CC5F19E21F4A55594EB91A3B179C6~-1~YAAQdetGaC/5mACDAQAAGmE0PwgaI2BjM7XC1v5bxLbnbSXEcXtWhjOBOHl2MY4cONldQGAfBKt57s1NwFQKRdxu3UhLysMNG09338z7Pgo6ibVJl4DH3EH5NvLA2wppjtLy9LRRjTxAQRxU2meMU8m7zgtx1Uwoa9G4Me9iJ26cIaI+UeMAjbkxFwiQCgeQgqlO68iAzPwlv8WfMFvvj1pivo84XMFhPst0SpLHRnK/tYw+/hVSHApsmYUeJpEA7jGQSQz23pmN/G/fVUYB6Es9G9j3WiKztiqft6+KwwGTCH5PVXTHqEVbiDZbliwWjaCBVP0cc2l7VNZQfHiK5xgOmEAaZPYOOBiFzp94dNzkFv4HKg133H8fOW0G4kA/bugVOmfyRzMIqHEYTtgERT2n~0~-1~-1',
+                'origin: https://www.ups.com',
+                'referer: https://www.ups.com/track?loc=en_US&tracknum=92748903029216543405818998&requester=ST/trackdetails',
+                'sec-ch-ua: "Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+                'sec-ch-ua-mobile: ?0',
+                'sec-ch-ua-platform: "Windows"',
+                'sec-fetch-dest: empty',
+                'sec-fetch-mode: cors',
+                'sec-fetch-site: same-origin',
+                'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+                'x-xsrf-token: CfDJ8Jcj9GhlwkdBikuRYzfhrpLnLyLrXcKf4wpS54hFHt1dFKC-kwZMNbI8B1wxFM27SaJkTCscMA5RHdXIWXgoPPt052ZR3SRTkvUav2280jdwhGUicV-8ztCKtiJiZOe7HjcD6aHk_OtEc5Cvu6f1x4k',
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $tracking_id = json_decode($response, true);
+
+        return $tracking_id;
+    }
 }
