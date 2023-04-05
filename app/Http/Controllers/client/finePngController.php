94674dd95e7bcd45a7eb3bdebc84c8598c457171
minhchinh93 <68152682+minhchinh93@users.noreply.github.com>
2023-04-05 22:13:43 +0700

    chinh update

--- app/Http/Controllers/client/finePngController.php
+++ app/Http/Controllers/client/finePngController.php
@@ -37,7 +37,7 @@ class finePngController extends Controller
             ))
             ->Where('product_png_details.Sku', 'like', "%{$keyword}%")
             ->orWhere('title', 'like', "%{$keyword}%")
-            ->orderBy('products.id', 'DESC')
+            ->orderBy('products.id', 'ASC')
             ->paginate(32);
 
         return view('client.findPNG.indexPNG', ['shows' => $show,
